74179b07332f3dc85eb85630a9eb0c6e0d0ef3e3
Evan Chih-Yu Lin <evan.linjin@gmail.com>
2017-04-10 22:31:51 +1200

    More work.

--- distributor.go
+++ distributor.go
@@ -5,11 +5,66 @@ import "sync"
 type Distributor struct {
 	sync.Mutex
 	users map[UserID]*User
+	addU  chan *User
+	remU  chan UserID
+	msgs  chan *Message
+	quit  chan bool
 }
 
 func NewDistributor() *Distributor {
 	d := Distributor{
 		users: make(map[UserID]*User),
+		addU:  make(chan *User),
+		remU:  make(chan UserID),
+		msgs:  make(chan *Message),
+		quit:  make(chan bool),
 	}
+	d.runService()
 	return &d
+}
+
+func (d *Distributor) AddUser(uid UserID) *User {
+	u := NewUser(uid)
+	d.addU <- u
+	return u
+}
+
+func (d *Distributor) RemoveUser(uid UserID) {
+	d.remU <- uid
+}
+
+func (d *Distributor) GetUsers(uids ...UserID) (res chan *User) {
+	res = make(chan *User)
+	go func() {
+		d.Lock()
+		for _, uid := range uids {
+			u, _ := d.users[uid]
+			res <- u
+		}
+		d.Unlock()
+	}()
+	return res
+}
+
+func (d *Distributor) runService() {
+	for {
+		select {
+		case u := <-d.addU:
+			d.Lock()
+			d.users[u.id] = u
+			d.Unlock()
+
+		case i := <-d.remU:
+			d.Lock()
+			delete(d.users, i)
+			d.Unlock()
+
+		case <-d.quit:
+			return
+		}
+	}
+}
+
+func (d *Distributor) endService() {
+	d.quit <- true
 }
--- user.go
+++ user.go
@@ -8,66 +8,71 @@ type User struct {
 	sync.RWMutex
 	id       UserID
 	sessions []*Session
-	addS     chan SessionID
+	addS     chan *Session
 	remS     chan SessionID
+	msgs     chan *Message
 	quit     chan bool
 }
 
 func NewUser(id UserID) *User {
 	u := User{
 		id:   id,
-		addS: make(chan SessionID),
+		addS: make(chan *Session),
 		remS: make(chan SessionID),
+		msgs: make(chan *Message),
 		quit: make(chan bool),
 	}
-	u.runService()
+	go u.runService()
 	return &u
 }
 
-func (u *User) AddSession(id SessionID) {
-	u.addS <- id
+func (u *User) AddSession(id SessionID) *Session {
+	s := NewSession(id)
+	u.addS <- s
+	return s
 }
 
 func (u *User) RemoveSession(id SessionID) {
 	u.remS <- id
 }
 
+func (u *User) SendMsg(msg *Message) {
+	u.msgs <- msg
+}
+
 func (u *User) runService() {
-	go func() {
-		for {
-		UserRunServiceSelect:
-			select {
-			case sid := <-u.addS:
-				for _, s := range u.sessions {
-					if s.id == sid {
-						goto UserRunServiceSelect
-					}
-				}
-				u.RLock()
-				u.sessions = append(u.sessions, NewSession(sid))
-				u.RUnlock()
+	for {
+		select {
+		case s := <-u.addS:
+			u.RLock()
+			u.sessions = append(u.sessions, s)
+			u.RUnlock()
 
-			case sid := <-u.remS:
-				for i, s := range u.sessions {
-					if s.id == sid {
-						u.RLock()
-						// Removes element 'i'.
-						u.sessions[i] = u.sessions[len(u.sessions)-1]
-						u.sessions = u.sessions[:len(u.sessions)-1]
-						break
-						u.RUnlock()
-					}
+		case sid := <-u.remS:
+			for i, s := range u.sessions {
+				if s.id == sid {
+					u.RLock()
+					// Removes element 'i'.
+					u.sessions[i] = u.sessions[len(u.sessions)-1]
+					u.sessions = u.sessions[:len(u.sessions)-1]
+					u.RUnlock()
+					break
 				}
-
-			case <-u.quit:
-				return
 			}
+
+		case msg := <-u.msgs:
+			u.Lock()
+			for _, s := range u.sessions {
+				s.SendMsg(msg)
+			}
+			u.Unlock()
+
+		case <-u.quit:
+			return
 		}
-	}()
+	}
 }
 
 func (u *User) endService() {
-	go func() {
-		u.quit <- true
-	}()
+	u.quit <- true
 }
