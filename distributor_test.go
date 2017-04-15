a5e876c7c9d7f55796249e47fefed31874f62b46
Evan Chih-Yu Lin <evan.linjin@gmail.com>
2017-04-15 12:36:40 +1200

    Improved tests.

--- distributor_test.go
+++ distributor_test.go
@@ -5,12 +5,35 @@ import (
 	"strconv"
 	"testing"
 	"time"
+	"sync"
 )
+
+type Counter struct {
+	sync.Mutex
+	n int64
+}
+
+func NewCounter() *Counter {
+	return &Counter{n:0}
+}
+
+func (c *Counter) Increment() {
+	c.Lock()
+	defer c.Unlock()
+	c.n += 1
+}
+
+func (c *Counter) GetCount() int64 {
+	c.Lock()
+	defer c.Unlock()
+	return c.n
+}
 
 func makeNewUserWithSessions(
 	distributor *Distributor,
 	uid UserID,
 	nSessions int,
+	msgCounter *Counter,
 ) (
 	user *User,
 	quitFunc func(),
@@ -19,23 +42,25 @@ func makeNewUserWithSessions(
 	sessions := make([]*Session, nSessions)
 	quitChan := make(chan struct{})
 
+	f := func(s *Session, q chan struct{}, c *Counter) {
+		for {
+			select {
+			case msg := <-s.C:
+				fmt.Printf("[%s] Message: %v\n", s.id, msg.data)
+				c.Increment()
+			case <-q:
+				fmt.Printf("[Listener] %s : listener ended.\n", s.id)
+				return
+			}
+		}
+	}
+
 	for i := 0; i < nSessions; i++ {
 		sessions[i] = user.AddSession(
 			SessionID(string(uid) + "_session" + strconv.Itoa(i)),
 		)
 		sessions[i].SetStatus(Online)
-
-		go func(sid SessionID, c chan *Message, q chan struct{}){
-			for {
-				select {
-				case msg := <-c:
-					fmt.Printf("[%s] Message: %v\n", sid, msg.data)
-				case <-q:
-					fmt.Printf("[Listener] %s : listener ended.\n", sid)
-					return
-				}
-			}
-		}(sessions[i].id, sessions[i].C, quitChan)
+		go f(sessions[i], quitChan, msgCounter)
 	}
 
 	quitFunc = func() {
@@ -52,20 +77,28 @@ func makeNewUserWithSessions(
 
 func TestDistributor_AddUser(t *testing.T) {
 	d := NewDistributor()
-	_, q := makeNewUserWithSessions(d, "user0", 10)
+	c := NewCounter()
+	u, q := makeNewUserWithSessions(d, "user0", 10, c)
+	m := u.NewMsg(u.id, "Test message.")
+	u.ReceiveMsg(m)
+	<-time.NewTimer(time.Second).C
 	d.EndAll()
 	q()
 	<-time.NewTimer(time.Second).C
+	if c.GetCount() != 10 {
+		t.Error("c.GetCount() != 10", "[GOT]", c.GetCount())
+	}
 }
 
 func TestDistributor_AddUser2(t *testing.T) {
 	const nUsers = 10
+	c := NewCounter()
 	d := NewDistributor()
 	users := make([]*User, nUsers)
 	quits := make([]func(), nUsers)
 	for i := 0; i < nUsers; i++ {
 		users[i], quits[i] = makeNewUserWithSessions(
-			d, UserID("user" + strconv.Itoa(i)), 1)
+			d, UserID("user" + strconv.Itoa(i)), 1, c)
 	}
 	d.EndAll()
 	for _, q := range quits {
